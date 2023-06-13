7605e7e03150f88a605c319f1cdbcde3956d38c6
AhmedZedan <ahmedzedan650@gmail.com>
2023-06-13 21:57:37 +0300

    add bash script to auto everything

---
+++ project.sh
@@ -0,0 +1,32 @@
+cd eks_infra
+terraform init
+terraform apply
+
+if [ $? -nq 0 ]
+then
+    echo "Failed to build the infrastructure!"
+    exit
+else
+  echo "Infrastructure has built successfully"
+fi
+
+cd ..
+
+
+if [ -f eks_infra/cluster_output.txt ];
+then
+  bastion_public_ip=$(awk -F: '{if($1=="bastion_public_ip") {print $2;exit}}' eks_infra/cluster_output.txt)
+	cluster_name=$(awk -F: '{if($1=="cluster_name") {print $2;exit}}' eks_infra/cluster_output.txt)
+else
+  echo "cluster_output file does not exist"
+fi
+
+
+cat > jenkins_deployment/inventory <<-EOF
+[bastion]
+$bastion_public_ip
+EOF
+
+echo "Starting jenkins deployment"
+
+ansible-playbook jenkins_deployment/playbook.yml -e "cluster_name=${cluster_name}" --ask-vault-pass
